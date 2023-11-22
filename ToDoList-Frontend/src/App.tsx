63d066b7f7aaf679b7fab9930401fd6d3984e87b
Yago Gomes <yaguamineral@gmail.com>
2023-11-22 10:21:57 -0300

    projeto iniciado

---
+++ ToDoList-Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from './pages/header/Header'
+import Section from './pages/section/Section'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Section/> 
+    </>
+  )
+}
+
+export default App
---
+++ ToDoList-Frontend/src/pages/section/Section.tsx
@@ -0,0 +1,19 @@
+import "./Section.css"
+
+export default function Section() {
+    return (
+        <section>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Desperte Sua Produtividade</h1>
+                        <p>Organize-se de maneira intuitiva, defina metas e conquiste seus objetivos diários com facilidade.
+                        </p>
+                        <button className="btn btn-danger">Crie sua conta</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
